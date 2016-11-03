9abf2b9c4b91e5dc29e022168cf566e79429fabe
liuzirui1122 <liuzirui1122@163.com>
2016-11-04 00:37:53 +0800

    前端修改
    -加入三色的提示
    -修改大屏"后台""公告"按钮显示两遍的bug
    -加入footer三个链接的response

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 
 
 $(function(){
+    
+    $("#terms").hide();
+    $("#instruction").hide();
     $(".timeBox").click(function(){
         var time;
         var date = $(this).parent(".collapsible-body").attr('id');
@@ -14,6 +17,30 @@ $(function(){
 
     });
     $('.known').click(function() {
-       $(this).parents(".card").hide(1000);
+       $(this).parents(".card").hide(500);
+    });
+    
+    $("#BTNinstruction").click(function(){
+        $("#instruction").slideToggle(500);
+        return;
+    });
+    
+    $("#BTNterms").click(function(){
+        $("#terms").slideToggle(500);
+        return;
+    });
+    $("#BTNfeedback").click(function(){
+        var $toastContent = $('<p>对本系统有建议或bug可以在易班应用详情页面评论！<br> 也可以联系我们邮箱liuzirui1122#163.com (#换成@)</p>');
+        Materialize.toast($toastContent, 5000);
+        return;
+    });
+    
+    $("#closeTerms").click(function(){
+        $("#terms").slideToggle(500);
+        return;
+    });
+    $("#closeInstruction").click(function(){
+        $("#instruction").slideToggle(500);
+        return;
     });
 });
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+
     session_start();
     include "assets/API/header_api_session.php";
     include "assets/API/iapp.php";
@@ -36,6 +38,8 @@
       exit('Could not connect: ' . mysql_error());
     }
     $db->query("set names 'utf8'");
+    
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -64,7 +68,7 @@
         <div class="nav-wrapper container hide-on-med-and-down">
           <a href="index.php" class="brand-logo"> <img class="logo circle" src="assets/img/logo.png" />健身房预约</a>
             <ul class="right">
-              <?php
+              <?php 
               if ($_SESSION['userType'] === "辅导员"){
                 echo "<li><a href='adminbroadcast.php'>发布公告</a></li>";
                 echo "<li><a href='adminform.php'>后台管理</a></li>";
@@ -84,8 +88,9 @@
   <main>
   <div class="container">
     <?php
+      
     if ($_SESSION['userType'] === "辅导员"){
-        echo "<br/><div class='row'>
+        echo "<br/><div class='row hide-on-large-only'>
         <div class='col s2 offset-s2 grid-example'>
         <a class='waves-effect waves-light btn' href='adminbroadcast.php' >发布公告</a>
         </div>";
@@ -126,6 +131,8 @@
          </div>
         </div>";
       }
+      
+                                                                        
      ?>
    <!-- 公告开始 -->
    <!-- 公告模板 php处理
@@ -152,13 +159,21 @@
     <p>您可以预订<b><?php echo $showingDay; ?></b>天内的健身,点击下方可用时间段进行预订!</p>
     <!-- <p>由于您之前两次预订失约，您在两周内不能再进行预约。 哦豁o(*≧▽≦)ツ┏━┓[拍桌狂笑!]</p> -->
     <!-- <p>您已经预约了目前所有可用的时间段！对不起，您一定是疯了- -</p> -->
+       
    </div>
+   
    <hr />
+      <div class="right">
+      <span class='colorBox teal lighten-2 showing'></span> 空闲
+      <span class='colorBox yellow darken-2 showing'></span> 繁忙
+      <span class='colorBox red lighten-3 showing'></span> 满额
+   </div>
+   <br>
    <!-- 预订状况开始  -->
    <!-- 以下为范例,括弧中为提示,正式上线时务必删除 -->
    <div id="bookList">
     <ul class="collapsible popout" data-collapsible="expandable">
-        <?php
+        <?php 
             for ($i=0;$i<$showingDay;$i++)
             {
                 $nowPeople = array();
@@ -230,7 +245,7 @@
                   </div>
                  </div>
                </li>";
-            }
+            } 
         ?>
     </ul>
    </div>
@@ -249,9 +264,9 @@
      <div class="col l4 offset-l2 s12">
       <h5 class="white-text">Links</h5>
       <ul>
-       <li><a class="grey-text text-lighten-3" href="#!">使用说明</a></li>
-       <li><a class="grey-text text-lighten-3" href="#!">使用条款</a></li>
-       <li><a class="grey-text text-lighten-3" href="#!">意见反馈</a></li>
+       <li><a id="BTNinstruction" class="grey-text text-lighten-3" href="#">使用说明</a></li>
+       <li><a id="BTNterms" class="grey-text text-lighten-3" href="#">使用条款</a></li>
+       <li><a id="BTNfeedback" class="grey-text text-lighten-3" href="#">意见反馈</a></li>
       </ul>
      </div>
     </div>
@@ -262,6 +277,44 @@
      <a class="grey-text text-lighten-3" href="http://buptyiban.org/">BUPTYiban</a>
     </div>
    </div>
+   <div id="terms">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">北京邮电大学沙河校区健身房管理规定</span>
+              <p>
+1. 学校健身房只对本校学生和教职工免费开放，谢绝校外人员进入。学生和教职工按照上述开放时间进入活动，进入时必须主动出示本人的校园一卡通，本人校园卡不得转借他人使用。<br/>
+2. 健身房内禁止吸烟，以确保场内卫生和空气流畅。严禁酒后进入健身房进行锻炼。<br/>
+3. 自觉维护健身房的公共卫生，严禁乱丢果皮纸屑，严禁在健身房内吸烟、乱丢垃圾或大声喧哗；严禁在墙壁乱涂乱画或蹬踏。<br/>
+4. 使用健身器材需穿运动服、鞋，禁止穿高跟鞋、皮鞋锻炼。<br/>
+5. 按说明书要求正确使用健身器材，确保人员及器材安全，防止使用不当造成不安全事故。<br/>
+6. 进入健身房参加运动的人员，要爱护健身器材，不得轻易拆卸器材；造成健身器材损坏的，照价赔偿。<br/>
+7. 健身器材发生故障或损坏，应停止使用，并及时报告管理人员进行维修。<br/>
+8. 进入健身房人员应提高警惕，注意安全，自行保管好衣物及重要财物，若有丢失责任自负。<br/>
+9. 活动完毕，自觉将各种器械放回原处，摆放整齐。严格遵守健身时间及健身房各注意事项。<br/>
+10.在健身房健身的人员必须遵守有关规章制度，服从管理人员的管理。<br/>
+</p>
+            </div>
+            <div class="card-action">
+              <a id="closeTerms" href="#">关闭</a>
+            </div>
+          </div>
+        </div>
+      <div id="instruction">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">健身房系统使用说明</span>
+              <p>
+预约: 进入健身房系统主页并经过易班授权后，只需点击可用的时间段(非红色满人时间段)即可进入预约页，在支付一定网薪(待定)后就预约完成啦。<br/>
+查看预约信息：手机端的话点左上角的头像进个人中心查看，电脑则是右上角。可以在这里临时取消预约。<br/>
+违约处罚：一旦被记录失约后，您将一段时间内无法预约。 如恶意违约多次可能会被封禁。<br/>
+
+</p>
+            </div>
+            <div class="card-action">
+              <a id="closeInstruction" href="#">关闭</a>
+            </div>
+          </div>
+        </div>
   </footer>
   <!--  Scripts-->
   <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
